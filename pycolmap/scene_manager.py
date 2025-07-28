bc1dc2803ecf0697f7e04a37e53f9dc8f3debe48
teju81 <teju81@gmail.com>
2025-07-28 16:55:52 +0530

    Update scene_manager.py

--- pycolmap/scene_manager.py
+++ pycolmap/scene_manager.py
@@ -19,7 +19,8 @@ from .rotation import Quaternion
 #-------------------------------------------------------------------------------
 
 class SceneManager:
-    INVALID_POINT3D = np.uint64(-1)
+    INVALID_POINT3D = np.uint64(-1 % (2**64))
+
 
     def __init__(self, colmap_results_folder, image_path=None):
         self.folder = colmap_results_folder
